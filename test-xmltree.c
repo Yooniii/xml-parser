ec346d19bf13d90abca468399cd133bcaf941a98
Sarah Yoon <111259968+Yooniii@users.noreply.github.com>
2024-04-07 12:28:47 -0400

    Create test-xmltree.c

---
+++ test-xmltree.c
@@ -0,0 +1,23 @@
+#include <assert.h>
+#include <stdlib.h>
+#include "tokenizer.h"
+#include "xml-tree.h"
+
+#include <stdio.h>
+int main(void) {
+    int len = 0;
+    char **tokens = tokenize_input(&len);
+    
+    struct xtree *tr = tree_create(tokens, len);
+
+    // Pretty-print the tree.
+    
+    //tree_print(tr);
+    tree_destroy(tr);
+
+    // Clean up token array.
+    for (int i = 0; i < len; ++i) {
+        free(tokens[i]);
+    }
+    free(tokens);
+}
